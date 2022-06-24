a5a4c940c0b1d5c975b6cbdb2351b7be74086403
muhrid44 <muhrid44167@gmail.com>
2022-06-24 11:14:00 +0700

    port changes, env updated

--- app.js
+++ app.js
@@ -484,10 +484,6 @@ app.get("/delete/:id", function(req,res){
 
 
 
-  let port = process.env.PORT;
-  if (port == null || port == "") {
-    port = 3000;
-  }
-  app.listen(port, function(){
-    console.log("connected");
-  });
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Welcome to port 3000!");
+  })
